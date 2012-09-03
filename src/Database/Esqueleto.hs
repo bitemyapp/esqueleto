dd417a98e2320217be04c4f7e9f0d89ad3e9284a
Felipe Lessa <felipe.lessa@gmail.com>
2012-09-03 16:26:42 -0300

    Some package re-exports from Esqueleto.

--- src/Database/Esqueleto.hs
+++ src/Database/Esqueleto.hs
@@ -35,7 +35,13 @@ module Database.Esqueleto
     -- * SQL backend
   , SqlQuery
   , select
+
+    -- * Re-exports
+  , module Database.Persist.Store
+  , module Database.Persist.GenericSql
   ) where
 
 import Database.Esqueleto.Internal.Language
 import Database.Esqueleto.Internal.Sql
+import Database.Persist.Store
+import Database.Persist.GenericSql
