d6d528d5a996b298dad13ed6d88bd62b3504be08
Danny B <danny.b.eml@gmail.com>
2013-08-09 14:56:19 +1100

    Fix underscore.

--- src/Database/Esqueleto.hs
+++ src/Database/Esqueleto.hs
@@ -44,7 +44,7 @@ module Database.Esqueleto
              , (==.), (>=.), (>.), (<=.), (<.), (!=.), (&&.), (||.)
              , (+.), (-.), (/.), (*.)
              , random_, round_, ceiling_, floor_
-             , min_, max_,_sum_, avg_
+             , min_, max_, sum_, avg_
              , like, (%), concat_, (++.)
              , subList_select, subList_selectDistinct, valList
              , in_, notIn, exists, notExists
