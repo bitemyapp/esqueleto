a4ebae23453b4958fcc9f90909b0de3ff43e780b
Fintan Halpenny <rossmanf@tcd.ie>
2017-08-02 16:34:46 +0100

    Use type constraint to constrain a to UTCTime

--- src/Database/Esqueleto/Internal/Language.hs
+++ src/Database/Esqueleto/Internal/Language.hs
@@ -53,6 +53,7 @@ import Text.Blaze.Html (Html)
 import qualified Data.ByteString as B
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
+import Data.Time.Clock (UTCTime)
 
 
 -- | Finally tagless representation of @esqueleto@'s EDSL.
@@ -352,7 +353,7 @@ class (Functor query, Applicative query, Monad query) =>
 
 
   random_  :: (PersistField a, Num a) => expr (Value a)
-  now_     :: (PersistField a)        => expr (Value a)
+  now_     :: (PersistField a, a ~ UTCTime) => expr (Value a)
   round_   :: (PersistField a, Num a, PersistField b, Num b) => expr (Value a) -> expr (Value b)
   ceiling_ :: (PersistField a, Num a, PersistField b, Num b) => expr (Value a) -> expr (Value b)
   floor_   :: (PersistField a, Num a, PersistField b, Num b) => expr (Value a) -> expr (Value b)
