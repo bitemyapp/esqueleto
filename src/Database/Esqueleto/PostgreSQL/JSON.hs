65cbcbe3355b3b4504e5869a735384a8f3988904
Felix Paulusma <felix.paulusma@gmail.com>
2019-07-28 19:16:36 +0200

    finished adding all JSON operators

--- src/Database/Esqueleto/PostgreSQL/JSON.hs
+++ src/Database/Esqueleto/PostgreSQL/JSON.hs
@@ -9,13 +9,16 @@
       * The @Type@ column in the PostgreSQL documentation tables
       are the types of the right operand, the left is always @jsonb@.
       * This module also exports 'PersistField' and 'PersistFieldSql'
-      instances for 'Data.Aeson.Value'
+      orphan instances for 'Data.Aeson.Value'
       * Since these operators can all take @NULL@ values as their input,
       and most can also output @NULL@ values (even when the inputs are
       guaranteed to not be NULL), all 'Data.Aeson.Value's are wrapped in
       'Maybe'. This also makes it easier to chain them.
       Just use the 'just' function to lift any non-'Maybe' JSON values
       in case it doesn't type check.
+      * As long as the previous operator's resulting value is
+      'Maybe' 'Data.Aeson.Value', any other JSON operator can be
+      used to transform the JSON further.
 
   /The PostgreSQL version the functions work with are included/
   /in their description./
@@ -68,7 +71,7 @@ module Database.Esqueleto.PostgreSQL.JSON
   , (?.)
   , (?|.)
   , (?&.)
-  -- * Deletion operators
+  -- * Deletion and concatenation operators
   --
   -- /Better documentation included with individual functions/
   --
@@ -79,6 +82,7 @@ module Database.Esqueleto.PostgreSQL.JSON
   -- @
   --     | Type    | Description                                                            |  Example
   -- ----+---------+------------------------------------------------------------------------+-------------------------------------------------
+  --  || | jsonb   | Concatenate two jsonb values into a new jsonb value                    | '["a", "b"]'::jsonb || '["c", "d"]'::jsonb
   --  -  | text    | Delete key/value pair or string element from left operand.             | '{"a": "b"}'::jsonb - 'a'
   --     |         | Key/value pairs are matched based on their key value.                  |
   --  -  | text[]  | Delete multiple key/value pairs or string elements from left operand.  | '{"a": "b", "c": "d"}'::jsonb - '{a,c}'::text[]
@@ -88,6 +92,7 @@ module Database.Esqueleto.PostgreSQL.JSON
   --  #- | text[]  | Delete the field or element with specified path                        | '["a", {"b":1}]'::jsonb #- '{1,b}'
   --     |         | (for JSON arrays, negative integers count from the end)                |
   -- @
+  , (||.)
   , (-.)
   , (#-.)
   ) where
@@ -133,7 +138,9 @@ infixl 6 -., #-.
 -- @
 --
 -- /Since: 3.1.0/
-(->.) :: SqlExpr (Value (Maybe Aeson.Value)) -> Either Int Text -> SqlExpr (Value (Maybe Aeson.Value))
+(->.) :: SqlExpr (Value (Maybe Aeson.Value))
+      -> Either Int Text
+      -> SqlExpr (Value (Maybe Aeson.Value))
 (->.) value (Right txt) = unsafeSqlBinOp " -> " value $ val txt
 (->.) value (Left i)    = unsafeSqlBinOp " -> " value $ val i
 
@@ -152,7 +159,9 @@ infixl 6 -., #-.
 -- @
 --
 -- /Since: 3.1.0/
-(->>.) :: SqlExpr (Value (Maybe Aeson.Value)) -> Either Int Text -> SqlExpr (Value (Maybe Text))
+(->>.) :: SqlExpr (Value (Maybe Aeson.Value))
+       -> Either Int Text
+       -> SqlExpr (Value (Maybe Text))
 (->>.) value (Right txt) = unsafeSqlBinOp " ->> " value $ val txt
 (->>.) value (Left i)    = unsafeSqlBinOp " ->> " value $ val i
 
@@ -194,7 +203,9 @@ infixl 6 -., #-.
 -- @
 --
 -- /Since: 3.1.0/
-(#>.) :: SqlExpr (Value (Maybe Aeson.Value)) -> [Text] -> SqlExpr (Value (Maybe Aeson.Value))
+(#>.) :: SqlExpr (Value (Maybe Aeson.Value))
+      -> [Text]
+      -> SqlExpr (Value (Maybe Aeson.Value))
 (#>.) value = unsafeSqlBinOp " #> " value . mkTextArray
 
 
@@ -211,7 +222,9 @@ infixl 6 -., #-.
 -- @
 --
 -- /Since: 3.1.0/
-(#>>.) :: SqlExpr (Value (Maybe Aeson.Value)) -> [Text] -> SqlExpr (Value (Maybe Text))
+(#>>.) :: SqlExpr (Value (Maybe Aeson.Value))
+       -> [Text]
+       -> SqlExpr (Value (Maybe Text))
 (#>>.)  value = unsafeSqlBinOp " #>> " value . mkTextArray
 
 -- | This operator checks for the JSON value on the right to be a subset
@@ -232,7 +245,9 @@ infixl 6 -., #-.
 -- @
 --
 -- /Since: 3.1.0/
-(@>.) :: SqlExpr (Value (Maybe Aeson.Value)) -> SqlExpr (Value (Maybe Aeson.Value)) -> SqlExpr (Value Bool)
+(@>.) :: SqlExpr (Value (Maybe Aeson.Value))
+      -> SqlExpr (Value (Maybe Aeson.Value))
+      -> SqlExpr (Value Bool)
 (@>.) = unsafeSqlBinOp " @> "
 
 -- | This operator works the same as '@>.', just with the arguments flipped.
@@ -253,7 +268,9 @@ infixl 6 -., #-.
 -- @
 --
 -- /Since: 3.1.0/
-(<@.) :: SqlExpr (Value (Maybe Aeson.Value)) -> SqlExpr (Value (Maybe Aeson.Value)) -> SqlExpr (Value Bool)
+(<@.) :: SqlExpr (Value (Maybe Aeson.Value))
+      -> SqlExpr (Value (Maybe Aeson.Value))
+      -> SqlExpr (Value Bool)
 (<@.) = unsafeSqlBinOp " <@ "
 
 -- | This operator checks if the given text is a top-level member of the
@@ -275,7 +292,9 @@ infixl 6 -., #-.
 -- @
 --
 -- /Since: 3.1.0/
-(?.) :: SqlExpr (Value (Maybe Aeson.Value)) -> Text -> SqlExpr (Value Bool)
+(?.) :: SqlExpr (Value (Maybe Aeson.Value))
+     -> Text
+     -> SqlExpr (Value Bool)
 (?.) value = unsafeSqlBinOp " ?? " value . val
 
 -- | This operator checks if __ANY__ of the given texts is a top-level member
@@ -297,7 +316,9 @@ infixl 6 -., #-.
 -- @
 --
 -- /Since: 3.1.0/
-(?|.) :: SqlExpr (Value (Maybe Aeson.Value)) -> [Text] -> SqlExpr (Value Bool)
+(?|.) :: SqlExpr (Value (Maybe Aeson.Value))
+      -> [Text]
+      -> SqlExpr (Value Bool)
 (?|.) value = unsafeSqlBinOp " ??| " value . mkTextArray
 
 -- | This operator checks if __ALL__ of the given texts are top-level members
@@ -319,34 +340,109 @@ infixl 6 -., #-.
 -- @
 --
 -- /Since: 3.1.0/
-(?&.) :: SqlExpr (Value (Maybe Aeson.Value)) -> [Text] -> SqlExpr (Value Bool)
+(?&.) :: SqlExpr (Value (Maybe Aeson.Value))
+      -> [Text]
+      -> SqlExpr (Value Bool)
 (?&.) value = unsafeSqlBinOp " ??& " value . mkTextArray
+
+-- | This operator concatenates two JSON values. The behaviour is
+-- self-evident when used on two arrays, but the behaviour on different
+-- combinations of JSON values might behave unexpectedly.
+--
+-- __CAUTION: THIS FUNCTION THROWS AN EXCEPTION WHEN CONCATENATING__
+-- __A JSON OBJECT WITH A JSON SCALAR VALUE!__
+--
+-- === __Arrays__
+--
+-- This operator is a standard concatenation function when used on arrays:
+--
+-- @
+-- [1,2]   || [2,3]   == [1,2,2,3]
+-- []      || [1,2,3] == [1,2,3]
+-- [1,2,3] || []      == [1,2,3]
+-- @
+--
+-- === __Objects__
+-- When concatenating JSON objects with other JSON objects, the fields
+-- from the JSON object on the right are added to the JSON object on the
+-- left. When concatenating a JSON object with a JSON array, the object
+-- will be inserted into the array; either on the left or right, depending
+-- on the position relative to the operator.
+--
+-- When concatening an object with a scalar value, an exception is thrown.
+--
+-- @
+-- {"a": 3.14}                    || {"b": true}         == {"a": 3.14, "b": true}
+-- {"a": "b"}                     || {"a": null}         == {"a": null}
+-- {"a": {"b": true, "c": false}} || {"a": {"b": false}} == {"a": {"b": false}}
+-- {"a": 3.14}                    || [1,null]            == [{"a": 3.14},1,null]
+-- [1,null]                       || {"a": 3.14}         == [1,null,{"a": 3.14}]
+-- 1                              || {"a": 3.14}         == ERROR: invalid concatenation of jsonb objects
+-- @
+--
+-- === __Scalar values__
+--
+-- Scalar values can be thought of as being singleton arrays when
+-- used with this operator. This rule does not apply when concatenating
+-- with JSON objects.
+--
+-- @
+-- 1          || null       == [1,null]
+-- true       || "a"        == [true,"a"]
+-- [1,2]      || false      == [1,2,false]
+-- null       || [1,"a"]    == [null,1,"a"]
+-- {"a":3.14} || true       == ERROR: invalid concatenation of jsonb objects
+-- {"a":3.14} || [true]     == [{"a":3.14},true]
+-- [false]    || {"a":3.14} == [false,{"a":3.14}]
+-- @
+--
+-- === __PostgreSQL Documentation__
+--
+-- /Works with any PostgreSQL of version >= 9.5/
+--
+-- @
+--     | Type  | Description                                         |  Example
+-- ----+-------+-----------------------------------------------------+--------------------------------------------
+--  || | jsonb | Concatenate two jsonb values into a new jsonb value | '["a", "b"]'::jsonb || '["c", "d"]'::jsonb
+-- @
+--
+-- /Note: The @||@ operator concatenates the elements at the top level of/
+-- /each of its operands. It does not operate recursively. For example, if/
+-- /both operands are objects with a common key field name, the value of the/
+-- /field in the result will just be the value from the right hand operand./
+--
+-- @since 3.1.0
+(||.) :: SqlExpr (Value (Maybe Aeson.Value))
+      -> SqlExpr (Value (Maybe Aeson.Value))
+      -> SqlExpr (Value (Maybe Aeson.Value))
+(||.) = unsafeSqlBinOp " || "
 
 -- | This operator can remove keys from an object, or string elements from an array
 -- when using text, and remove certain elements by index when using integers.
 --
 -- __CAUTION: THIS FUNCTION THROWS AN EXCEPTION WHEN USED ON ANYTHING OTHER__
 -- __THAN OBJECTS OR ARRAYS WHEN USING TEXT, AND ANYTHING OTHER THAN ARRAYS__
--- __WHEN USING INTEGERS__
+-- __WHEN USING INTEGERS!__
 --
 -- === __Objects__
 --
 -- @
--- {"a": 3.14}            - []         == {"a": 3.14}
--- {"a": 3.14}            - ["a"]      == {}
--- {"a": "b"}             - ["b"]      == {"a": "b"}
--- {"a": 3.14}            - ["a","b"]  == {}
--- {"a": 3.14, "c": true} - ["a","b"]  == {"c": true}
+-- {"a": 3.14}            - []          == {"a": 3.14}
+-- {"a": 3.14}            - ["a"]       == {}
+-- {"a": "b"}             - ["b"]       == {"a": "b"}
+-- {"a": 3.14}            - ["a","b"]   == {}
+-- {"a": 3.14, "c": true} - ["a","b"]   == {"c": true}
 -- ["a", 2, "c"]          - ["a","b"]   == [2, "c"] -- can remove strings from arrays
 -- [true, "b", 5]         - 0           == ["b", 5]
 -- [true, "b", 5]         - 3           == [true, "b", 5]
 -- [true, "b", 5]         - -1          == [true, "b"]
 -- [true, "b", 5]         - -4          == [true, "b", 5]
 -- []                     - 1           == []
--- 1                      - {anything}  == ERROR: cannot delete from scalar
--- "a"                    - {anything}  == ERROR: cannot delete from scalar
--- true                   - {anything}  == ERROR: cannot delete from scalar
--- null                   - {anything}  == ERROR: cannot delete from scalar
+-- {"1": true}            - 1           == ERROR: cannot delete from object using integer index
+-- 1                      - <anything>  == ERROR: cannot delete from scalar
+-- "a"                    - <anything>  == ERROR: cannot delete from scalar
+-- true                   - <anything>  == ERROR: cannot delete from scalar
+-- null                   - <anything>  == ERROR: cannot delete from scalar
 -- @
 --
 -- === __PostgreSQL Documentation__
@@ -365,7 +461,9 @@ infixl 6 -., #-.
 -- @
 --
 -- /Since: 3.1.0/
-(-.) :: SqlExpr (Value (Maybe Aeson.Value)) -> Either Int [Text] -> SqlExpr (Value Bool)
+(-.) :: SqlExpr (Value (Maybe Aeson.Value))
+     -> Either Int [Text]
+     -> SqlExpr (Value (Maybe Aeson.Value))
 (-.) value (Right ts) = unsafeSqlBinOp " - " value $ mkTextArray ts
 (-.) value (Left i) = unsafeSqlBinOp " - " value $ val i
 
@@ -416,7 +514,9 @@ infixl 6 -., #-.
 -- @
 --
 -- /Since: 3.1.0/
-(#-.) :: SqlExpr (Value (Maybe Aeson.Value)) -> [Text] -> SqlExpr (Value Bool)
+(#-.) :: SqlExpr (Value (Maybe Aeson.Value))
+      -> [Text]
+      -> SqlExpr (Value (Maybe Aeson.Value))
 (#-.) value = unsafeSqlBinOp " #- " value . mkTextArray
 
 mkTextArray :: [Text] -> SqlExpr (Value Text)
@@ -465,12 +565,11 @@ fromPersistValueError databaseType received = T.concat
     ]
 
 fromPersistValueParseError
-  :: (Show a)
-  => a -- ^ Received value
+  :: Text -- ^ Received value
   -> Text -- ^ Additional error
   -> Text -- ^ Error message
 fromPersistValueParseError received err = T.concat
     [ "Failed to parse Haskell type `Aeson.Value`, "
-    , "but received ", T.pack (show received)
+    , "but received ", received
     , " | with error: ", err
     ]
