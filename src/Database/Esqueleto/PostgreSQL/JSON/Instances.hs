b31728982c40f8c0100cbbc8741f69a7d5ee435f
Felix Paulusma <felix.paulusma@gmail.com>
2019-07-31 19:35:12 +0200

    added JSONB newtype with instances, instead of orphaning Aeson.Value

--- src/Database/Esqueleto/PostgreSQL/JSON/Instances.hs
+++ src/Database/Esqueleto/PostgreSQL/JSON/Instances.hs
@@ -1,44 +1,68 @@
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# OPTIONS_GHC -Wno-orphans #-}
 module Database.Esqueleto.PostgreSQL.JSON.Instances where
 
-import Data.Aeson (encode, eitherDecodeStrict)
-import qualified Data.Aeson as Aeson (Value)
+import Data.Aeson (FromJSON(..), ToJSON(..), encode, eitherDecodeStrict)
+import Data.Bifunctor (first)
 import qualified Data.ByteString.Lazy as BSL (toStrict)
 import Data.Text (Text)
 import qualified Data.Text as T (concat, pack)
 import qualified Data.Text.Encoding as TE (decodeUtf8, encodeUtf8)
+import Database.Esqueleto (Value, just, val)
 import Database.Esqueleto.Internal.PersistentImport
+import Database.Esqueleto.Internal.Sql (SqlExpr)
+import GHC.Generics (Generic)
 
--- Mainly copied over from Database.Persist.Postgresql.JSON
--- Since we don't need anything else and adding another dependency
--- just for these two instances is a bit overkill.
+
+-- | Newtype wrapper around any type with a JSON representation.
+--
+-- @since 3.1.0
+newtype JSONB a = JSONB { unJSONB :: a }
+  deriving
+    ( Generic
+    , FromJSON
+    , ToJSON
+    , Eq
+    , Foldable
+    , Functor
+    , Ord
+    , Read
+    , Show
+    , Traversable
+    )
+
+type JSONExpr a = SqlExpr (Value (Maybe (JSONB a)))
+
+-- | Convenience function to lift a regular value into
+-- a 'JSONB' expression.
+jsonbVal :: (FromJSON a, ToJSON a) => a -> JSONExpr a
+jsonbVal = just . val . JSONB
 
 -- | @since 3.1.0
-instance PersistField Aeson.Value where
-  toPersistValue = PersistDbSpecific . BSL.toStrict . encode
-  fromPersistValue pVal = case pVal of
-      PersistByteString bs -> fromLeft (badParse $ TE.decodeUtf8 bs) $ eitherDecodeStrict bs
-      PersistText t -> fromLeft (badParse t) $ eitherDecodeStrict (TE.encodeUtf8 t)
+instance (FromJSON a, ToJSON a) => PersistField (JSONB a) where
+  toPersistValue = PersistDbSpecific . BSL.toStrict . encode . unJSONB
+  fromPersistValue pVal = fmap JSONB $ case pVal of
+      PersistByteString bs -> first (badParse $ TE.decodeUtf8 bs) $ eitherDecodeStrict bs
+      PersistText t -> first (badParse t) $ eitherDecodeStrict (TE.encodeUtf8 t)
       x -> Left $ fromPersistValueError "string or bytea" x
 
 -- | jsonb - @since 3.1.0
-instance PersistFieldSql Aeson.Value where
+instance (FromJSON a, ToJSON a) => PersistFieldSql (JSONB a) where
   sqlType _ = SqlOther "JSONB"
 
 badParse :: Text -> String -> Text
 badParse t = fromPersistValueParseError t . T.pack
-
-fromLeft :: (a -> b) -> Either a x -> Either b x
-fromLeft f (Left a) = Left $ f a
-fromLeft _ (Right r) = Right r
 
 fromPersistValueError
   :: Text -- ^ Database type(s), should appear different from Haskell name, e.g. "integer" or "INT", not "Int".
   -> PersistValue -- ^ Incorrect value
   -> Text -- ^ Error message
 fromPersistValueError databaseType received = T.concat
-    [ "Failed to parse Haskell type `Aeson.Value`; "
+    [ "Failed to parse Haskell newtype `JSONB a`; "
     , "expected ", databaseType
     , " from database, but received: ", T.pack (show received)
     , ". Potential solution: Check that your database schema matches your Persistent model definitions."
@@ -49,7 +73,7 @@ fromPersistValueParseError
   -> Text -- ^ Additional error
   -> Text -- ^ Error message
 fromPersistValueParseError received err = T.concat
-    [ "Failed to parse Haskell type `Aeson.Value`, "
+    [ "Failed to parse Haskell type `JSONB a`, "
     , "but received ", received
     , " | with error: ", err
     ]
