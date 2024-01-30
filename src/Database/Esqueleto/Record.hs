28816847095a74abdba6c1c0fcb73e2ab80749ce
Christian Berg <christian.berg@msu-solutions.de>
2024-01-30 10:24:31 +0100

    fix build for ghc 9.8.1 (#386)

--- src/Database/Esqueleto/Record.hs
+++ src/Database/Esqueleto/Record.hs
@@ -214,10 +214,14 @@ data RecordInfo = RecordInfo
     -- cause problems, but it's easy to pass around so might as well.
     constraints :: Cxt
   , -- | The original record's type-variable-binders.
+#if MIN_VERSION_template_haskell(2,18,0)
+    typeVarBinders :: [TyVarBndr BndrVis]
+#else 
 #if MIN_VERSION_template_haskell(2,17,0)
     typeVarBinders :: [TyVarBndr ()]
 #else
     typeVarBinders :: [TyVarBndr]
+#endif
 #endif
   , -- | The original record's kind, I think.
     kind :: Maybe Kind
