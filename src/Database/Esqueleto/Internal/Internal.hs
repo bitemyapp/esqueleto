98deaea5c217668f65488a6dcd6710361cb5fc17
parsonsmatt <parsonsmatt@gmail.com>
2024-12-30 12:04:28 -0700

    forgot about joinv

--- src/Database/Esqueleto/Internal/Internal.hs
+++ src/Database/Esqueleto/Internal/Internal.hs
@@ -694,7 +694,10 @@ nothing = unsafeSqlValue "NULL"
 
 -- | Join nested 'Maybe's in a 'Value' into one. This is useful when
 -- calling aggregate functions on nullable fields.
-joinV :: SqlExpr (Value (Maybe (Maybe typ))) -> SqlExpr (Value (Maybe typ))
+joinV
+    :: NullableFieldProjection typ typ'
+    => SqlExpr (Value (Maybe typ))
+    -> SqlExpr (Value (Maybe typ'))
 joinV = veryUnsafeCoerceSqlExprValue
 
 
